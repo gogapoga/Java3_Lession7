aff1353dda795802b57ef28e6e694c1e12294087
gogapoga <gogapoga1981@yandex,ru>
2018-12-13 22:49:23 +0300

    ДЗ к седьмому уроку

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -3,6 +3,17 @@ package ru.gb;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Tester.start("ru.gb.CalcTest1");
+	    try {
+             Tester.start("ru.gb.CalcTest2");
+        } catch (RuntimeException e) {
+	        e.printStackTrace();
+        }
+        try {
+             Tester.start("ru.gb.CalcTest3");
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        Tester.start("ru.gb.CalcTest4");
     }
 }
